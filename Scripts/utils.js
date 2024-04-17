6a5e0a4c4b260da158b71055582a3744eaa47635
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-04-17 16:04:30 -0500

    Agregue unos ejmeplos por para ver si podemos cambiar un poco el diseño del algoritmo

--- Scripts/utils.js
+++ Scripts/utils.js
@@ -1,8 +1,11 @@
+// Almacenar elementos del DOM en variables
 let input = document.getElementById("inputString");
-document.getElementById("reporte").addEventListener("click", desplegar);
 let tArea = document.getElementById("respuesta");
-document.getElementById("ingresar").addEventListener("click", function(){calcularScoreBoard(input.value);});
 let opcion = document.getElementById("criterio");
+
+// Agregar event listeners
+document.getElementById("reporte").addEventListener("click", desplegar);
+document.getElementById("ingresar").addEventListener("click", function(){calcularScoreBoard(input.value);});
 
 function calcularScoreBoard(caso){
     tArea.value = "";
@@ -31,3 +34,64 @@ function desplegar(){
         }).join('\n');
     }
 }
+
+/* 
+function calcularScoreBoard(caso){
+    let casos = caso.split(";");
+
+    tArea.value = casos.map(elemento => {
+        let resultado = elemento.split(" ");
+        resultado[1] = parseInt(resultado[1]);
+        resultado.sort((a, b) => a[1] - b[1]);
+        return `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}`;
+    }).join('\n');
+}
+
+function desplegar(){
+    let lineas = tArea.value.split('\n').filter(Boolean);
+
+    if(opcion.value == "problemsSolved"){
+        tArea.value = lineas.sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[1]);
+            const scoreB = parseInt(b.split(' ')[1]);
+            return scoreB - scoreA;
+        }).join('\n');
+    } else if(opcion.value == "penaltyTime"){
+        tArea.value = lineas.sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[2]);
+            const scoreB = parseInt(b.split(' ')[2]);
+            return scoreB - scoreA;
+        }).join('\n');
+    }
+} 
+*/
+
+
+/* 
+function calcularScoreBoard(caso){
+    let resultados = caso.split(";").map((elemento) => {
+        let resultado = elemento.split(" ");
+        resultado[1] = parseInt(resultado[1]);
+        resultado.sort((a, b) => a - b);
+        return `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}`;
+    });
+    tArea.value = resultados.join('\n');
+}
+
+function desplegar(){
+    let lineas = tArea.value.split('\n').filter(Boolean);
+    if(opcion.value == "problemsSolved"){
+        tArea.value = lineas.sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[1]);
+            const scoreB = parseInt(b.split(' ')[1]);
+            return scoreB - scoreA;
+        }).join('\n');
+    } else if(opcion.value == "penaltyTime"){
+        tArea.value = lineas.sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[2]);
+            const scoreB = parseInt(b.split(' ')[2]);
+            return scoreA - scoreB;
+        }).join('\n');
+    }
+} 
+*/
