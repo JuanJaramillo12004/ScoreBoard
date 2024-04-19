fe70aec5a0151f9dfddd4113d107d7bee8b228b7
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-04-19 18:55:43 -0500

    Si

--- Scripts/utils.js
+++ Scripts/utils.js
@@ -1,100 +1,39 @@
 /* Team1 1 5 C;Team2 5 10 C;Team3 2 15 I;Team1 3 20 R;Team2 1 25 R;Team2 4 30 C;Team1 3 35 I;Team1 3 40 C;Team3 2 45 I;Team3 2 50 C */
 
-
 // Almacenar elementos del DOM en variables
 let input = document.getElementById("inputString");
 let tArea = document.getElementById("respuesta");
 let opcion = document.getElementById("criterio");
 
 // Agregar event listeners
-document.getElementById("reporte").addEventListener("click", desplegar);
-document.getElementById("ingresar").addEventListener("click", function(){calcularScoreBoard(input.value);});
-
+document.getElementById("procesar").addEventListener("click", function(){calcularScoreBoard(input.value);});
+tArea.value = "";
 function calcularScoreBoard(caso){
-    tArea.value = "";
-    caso.split(";").forEach((elemento) => {
-        let resultado = elemento.split(" ");
-        resultado[1] = parseInt(resultado[1]);
-        resultado.sort((a, b) => a[1] - b[1]);
-        console.log(resultado); 
-        tArea.value += `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}\n`;
-    });
-    console.log(tArea.value);
-}
+    if (opcion.value == "ingresar"){
+        caso.split(";").forEach((elemento) => {
+            let resultado = elemento.split(" "); //elemento.trim().split(" "); 
+            tArea.value += `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}\n`;
+        });
 
-function desplegar(){
-    if(opcion.value == "problemsSolved"){
-        tArea.value = tArea.value.split('\n').filter(Boolean).sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[1]);
-            const scoreB = parseInt(b.split(' ')[1]);
+    } else if(opcion.value == "problemsSolved"){
+        tArea.value = tArea.value.split('\n').sort((a, b) => {
+            const scoreA = a.split(' ')[1];
+            const scoreB = b.split(' ')[1];
             return scoreB - scoreA;
         }).join('\n');
+
     } else if(opcion.value == "penaltyTime"){
-        tArea.value = tArea.value.split('\n').filter(Boolean).sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[2]);
-            const scoreB = parseInt(b.split(' ')[2]);
+        tArea.value = tArea.value.split('\n').sort((a, b) => {
+            const scoreA = a.split(' ')[2];
+            const scoreB = b.split(' ')[2]; 
             return scoreA - scoreB;
         }).join('\n');
-    }
-}
 
-/* 
-function calcularScoreBoard(caso){
-    let casos = caso.split(";");
-
-    tArea.value = casos.map(elemento => {
-        let resultado = elemento.split(" ");
-        resultado[1] = parseInt(resultado[1]);
-        resultado.sort((a, b) => a[1] - b[1]);
-        return `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}`;
-    }).join('\n');
-}
-
-function desplegar(){
-    let lineas = tArea.value.split('\n').filter(Boolean);
-
-    if(opcion.value == "problemsSolved"){
-        tArea.value = lineas.sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[1]);
-            const scoreB = parseInt(b.split(' ')[1]);
-            return scoreB - scoreA;
-        }).join('\n');
-    } else if(opcion.value == "penaltyTime"){
-        tArea.value = lineas.sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[2]);
-            const scoreB = parseInt(b.split(' ')[2]);
-            return scoreB - scoreA;
+    } else { 
+        tArea.value = tArea.value.split('\n').sort((a, b) => {
+            const teamA = a.split(' ')[0];
+            const teamB = b.split(' ')[0];
+            return teamA.localeCompare(teamB);
         }).join('\n');
     }
-} 
-*/
-
-
-/* 
-function calcularScoreBoard(caso){
-    let resultados = caso.split(";").map((elemento) => {
-        let resultado = elemento.split(" ");
-        resultado[1] = parseInt(resultado[1]);
-        resultado.sort((a, b) => a - b);
-        return `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}`;
-    });
-    tArea.value = resultados.join('\n');
 }
-
-function desplegar(){
-    let lineas = tArea.value.split('\n').filter(Boolean);
-    if(opcion.value == "problemsSolved"){
-        tArea.value = lineas.sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[1]);
-            const scoreB = parseInt(b.split(' ')[1]);
-            return scoreB - scoreA;
-        }).join('\n');
-    } else if(opcion.value == "penaltyTime"){
-        tArea.value = lineas.sort((a, b) => {
-            const scoreA = parseInt(a.split(' ')[2]);
-            const scoreB = parseInt(b.split(' ')[2]);
-            return scoreA - scoreB;
-        }).join('\n');
-    }
-} 
-*/
