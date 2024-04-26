97cc4119fd66b051c438f744f9d1c96bd4c0cf66
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-04-26 18:05:39 -0500

    2 Opciones una con array y otra con objetos

--- Scripts/utils.js
+++ Scripts/utils.js
@@ -5,33 +5,25 @@ let option = document.getElementById("options");
 document.getElementById("process").addEventListener("click", function(){calcularScoreBoard(input.value);});
 
 function calcularScoreBoard(caso){
-    // Creamos un objeto vacío para almacenar los equipos y sus puntajes
     let equipos = {};
 
-    // Dividimos el caso en elementos separados por ";"
     caso.split(";").forEach((element) => {
-        // Dividimos cada elemento en equipo, problema, tiempo y estado
         let [team, problem, time, status] = element.split(" ");
 
-        // Si el equipo no existe en el objeto equipos, lo agregamos con valores iniciales
         if (!(team in equipos)) {
             equipos[team] = {team, problem: 0, time: 0};
         }
 
-        // Si el estado es 'I' (incorrecto), sumamos 20 al tiempo del equipo
         if (status === 'I') {
             equipos[team].time += 20;
         }
 
-        // Si el estado es 'C' (correcto), incrementamos el contador de problemas del equipo
-        // y sumamos el tiempo al tiempo total del equipo
         if (status === 'C') {
             equipos[team].problem += 1;
             equipos[team].time += parseInt(time);
         }
     });
 
-    // Convertimos el objeto equipos en un array y lo ordenamos según las reglas de puntuación
     let equiposArray = Object.values(equipos);
     equiposArray.sort((a, b) => {
         if (b.problem !== a.problem) {
@@ -43,6 +35,5 @@ function calcularScoreBoard(caso){
         }
     });
 
-    // Imprimimos los resultados en el formato especificado
-    tArea.value = equiposArray.map(equipo => `${equipo.team} ${equipo.problem} ${equipo.time} C`).join('\n');
+    tArea.value = equiposArray.map(equipo => `${equipo.team} ${equipo.problem} ${equipo.time}`).join('\n');
 }
